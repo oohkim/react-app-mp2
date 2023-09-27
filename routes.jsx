1dd91dafc5a1bbcaa426ba4ac3aa459ca76db21b
oohkim <kimberlyanne0975@gmail.com>
2023-09-27 18:00:13 +0800

    Routes

---
+++ routes.jsx
@@ -0,0 +1,15 @@
+import Hero from './components/Hero/Hero';
+import Schedule from './pages/Schedule.Schedule';
+
+const routes = [
+    {
+        path: "/",
+        element: <Hero />,
+    },
+    {
+        path: "/schedule",
+        element: <Schedule />
+    }
+]
+
+export default routes;
