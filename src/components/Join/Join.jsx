018f3bae44d7d97c8342728c930707eb38f68d2f
oohkim <kimberlyanne0975@gmail.com>
2023-09-28 13:52:54 +0800

    Added function to Join.jsx

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -1,43 +1,66 @@
-import React, { useRef } from 'react'
-import './Join.css'
-import emailjs from '@emailjs/browser'
+import React, { useRef, useState } from "react";
+import "./Join.css";
+import emailjs from "@emailjs/browser";
 
 const Join = () => {
+  const form = useRef();
+  const [emailValue, setEmailValue] = useState("");
 
-const form = useRef ()
-
-const sendEmail = (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_gl1llva', 'template_8rlxaqr', form.current, 'VDKcHtxSmfAFQp7Fw')
-      .then((result) => {
-          console.log(result.text); 
-      }, (error) => {
+    emailjs
+      .sendForm(
+        "service_gl1llva",
+        "template_8rlxaqr",
+        form.current,
+        "VDKcHtxSmfAFQp7Fw"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
+
+    setEmailValue("");
+  };
+
+  const handleEmailChange = (e) => {
+    setEmailValue(e.target.value);
   };
 
   return (
     <div className="Join" id="join-us">
-        <div className="left-j">
-            <hr />
-            <div>
-                <span>READY TO</span>
-                <span> LEVEL UP</span>
-            </div>
-            <div>
-                <span>YOUR BODY</span>
-                <span> WITH US?</span>
-            </div> 
+      <div className="left-j">
+        <hr />
+        <div>
+          <span>READY TO</span>
+          <span> LEVEL UP</span>
         </div>
-        <div className="right-j">
-            <form ref={form} className="email-container" onSubmit={ sendEmail  }>
-                <input type="email" name="user_email" placeholder="Enter your email address" />
-                <button className="btn btn-j">Subscribe</button>
-            </form>
+        <div>
+          <span>YOUR BODY</span>
+          <span> WITH US?</span>
         </div>
+      </div>
+      <div className="right-j">
+        <form ref={form} className="email-container" onSubmit={sendEmail}>
+          <input
+            type="email"
+            name="user_email"
+            placeholder="Enter your email address"
+            value={emailValue}
+            onChange={handleEmailChange}
+          />
+          <button className="btn btn-j" type="submit">
+            Subscribe
+          </button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Join
+export default Join;
--- src/pages/Classes/classesData.jsx
+++ src/pages/Classes/classesData.jsx
@@ -2,12 +2,12 @@ const classesData = [
   {
     title: "Aerobics",
     description: "Join our high-energy aerobics class to boost your fitness!",
-    image: "path/to/aerobics-image.jpg", // Provide the actual image path
+    image: "path/to/aerobics-image.jpg",
   },
   {
     title: "Weight Lifting",
     description: "Build strength and muscle with our weight lifting sessions.",
-    image: "path/to/weight-lifting-image.jpg", // Provide the actual image path
+    image: "path/to/weight-lifting-image.jpg",
   },
 ];
 
